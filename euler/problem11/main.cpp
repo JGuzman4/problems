b1381e00bc67eb3fc942572c4df51c11d769e9a3
Jeff Guzman <jeff@liatrio.com>
2018-02-18 18:45:41 -0800

    finished problem 11

--- euler/problem11/main.cpp
+++ euler/problem11/main.cpp
@@ -12,32 +12,43 @@
 using namespace std;
 
 
-string number = "";
 
-void readGrid(int grid[20][20]);
-void printGrid(int grid[20][20]);
+void readGrid(unsigned long int grid[20][20]);
+void printGrid(unsigned long int grid[20][20]);
+unsigned long int upDown(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]);
+unsigned long int leftRight(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]);
+unsigned long int diagonally1(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]);
+unsigned long int diagonally2(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]);
 
-unsigned long int thirteenProduct(int index);
 
 int main(){
 
   unsigned long int greatest = 0;
-  int grid[20][20];
+  unsigned long int grid[20][20];
 
   readGrid(grid);
   printGrid(grid);
 
+  for (int i = 0; i < 16; i++){
+    for (int j = 0; j < 16; j++){
+      if (upDown(i, j, grid) > greatest)
+        greatest = upDown(i, j, grid);
+      if (leftRight(i, j, grid) > greatest)
+        greatest = leftRight(i, j, grid);
+      if (diagonally1(i, j, grid) > greatest)
+        greatest = diagonally1(i, j, grid);
+      if (diagonally2(i, j, grid) > greatest)
+        greatest = diagonally2(i, j, grid);
+    }
+  }
 
-  //for (int i = 0; i < number.length()-13; i++){
-    //if (thirteenProduct(i) > greatest)
-      //greatest = thirteenProduct(i);
-  //}
 
-  //cout << greatest << endl;
+
+  cout << greatest << endl;
   return 0;
 }
 
-void readGrid(int grid[20][20]){
+void readGrid(unsigned long int grid[20][20]){
   int n = 0;
   for (int i = 0; i < 20; i++){
     for (int j = 0; j < 20; j++){
@@ -47,7 +58,7 @@ void readGrid(int grid[20][20]){
   }
 }
 
-void printGrid(int grid[20][20]){
+void printGrid(unsigned long int grid[20][20]){
   for (int i = 0; i < 20; i++){
     for (int j = 0; j < 20; j++){
       cout << grid[i][j] << " ";
@@ -56,10 +67,16 @@ void printGrid(int grid[20][20]){
   }
 }
 
-unsigned long int thirteenProduct(int index){
-  unsigned long int product = (number[index] - '0');
-  for (int i = index+1; i < index + 13; i++){
-    product *= (number[i] - '0');
-  }
-  return product;
+
+unsigned long int upDown(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]){
+  return grid[i+3][j]*grid[i+2][j]*grid[i+1][j]*grid[i][j];
+}
+unsigned long int leftRight(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]){
+  return grid[i][j+3]*grid[i][j+2]*grid[i][j+1]*grid[i][j];
+}
+unsigned long int diagonally1(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]){
+  return grid[i+3][j+3]*grid[i+2][j+2]*grid[i+1][j+1]*grid[i][j];
+}
+unsigned long int diagonally2(unsigned long int i, unsigned long int j, unsigned long int grid[20][20]){
+  return grid[i][j+3]*grid[i+1][j+2]*grid[i+2][j+1]*grid[i+3][j];
 }
