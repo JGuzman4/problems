ee821c63a67ed2249f87539dc0188c6a9a8636dd
JGuzman4 <jguzman23@mail.csuchico.edu>
2018-07-28 16:09:14 -0700

    finished problem 31

--- euler/problem031/main.cpp
+++ euler/problem031/main.cpp
@@ -24,11 +24,15 @@
 using namespace std;
 using namespace boost::multiprecision;
 
+void print(vector<vector<int> > v);
+void resize(vector<vector<int> > &v, int total, vector<int> pences);
+int countWays(vector<vector<int> > &v, vector<int> pences);
 
 int main(int argc, char* argv[]){
 
-  vector<int> pences;
+  vector<vector<int> > ways;
 
+  vector<int> pences;
   pences.push_back(1);
   pences.push_back(2);
   pences.push_back(5);
@@ -36,14 +40,52 @@ int main(int argc, char* argv[]){
   pences.push_back(20);
   pences.push_back(50);
   pences.push_back(100);
+  pences.push_back(200);
 
+  int total = 200;
 
-  for (unsigned int i = 0; i < pences.size(); i++){
-    for (unsigned int j = 0; j < pences.size(); j++){
+  resize(ways, total, pences);
 
-    }
-  }
+  cout << countWays(ways, pences) << endl;
 
 
   return 0;
 } 
+
+void print(vector<vector<int> > v){
+  for (unsigned int i = 0; i < v.size(); i++){
+    for (unsigned int j = 0; j < v[i].size(); j++){
+      cout << v[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+}
+
+void resize(vector<vector<int> > &v, int total, vector<int> pences){
+
+  v.resize(pences.size());
+  for (unsigned int i = 0; i < v.size(); i++){
+    v[i].resize(total+1);
+  }
+  
+}
+
+int countWays(vector<vector<int> > &v, vector<int> pences){
+  for (unsigned int i = 0; i < v[0].size(); i++){
+    v[0][i] = 1;
+  }
+
+  for (unsigned int i = 1; i < v.size(); i++){
+    for (unsigned int j = 0; j < v[i].size(); j++){
+      if (j >= pences[i]){
+        v[i][j] = v[i-1][j] + v[i][j-pences[i]];
+      }
+      else {
+        v[i][j] = v[i-1][j];
+      }
+    }
+  }
+
+  return v[v.size()-1][v[0].size()-1];
+}
