00da759f55c9003ee8e2e482e8034a766c7e950d
jguzman4 <jeffg@liatrio.com>
2022-05-10 17:20:06 -0700

    plusMinus

--- hackerRank/plusMinus/main.cpp
+++ hackerRank/plusMinus/main.cpp
@@ -2,7 +2,15 @@
 
 using namespace std;
 
-vector<string> split_string(string);
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
 
 void plusMinus(vector<int> arr) {
 
@@ -26,65 +34,68 @@ void plusMinus(vector<int> arr) {
   cout << positive << endl;
   cout << negative << endl;
   cout << zero << endl;
-
 }
 
 int main()
 {
-  int n;
-  cin >> n;
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string n_temp;
+    getline(cin, n_temp);
 
-  string arr_temp_temp;
-  getline(cin, arr_temp_temp);
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-  vector<string> arr_temp = split_string(arr_temp_temp);
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
 
-  vector<int> arr(n);
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
 
-  for (int i = 0; i < n; i++) {
-    int arr_item = stoi(arr_temp[i]);
+    vector<int> arr(n);
 
-    arr[i] = arr_item;
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
 
-  }
+        arr[i] = arr_item;
+    }
 
-  plusMinus(arr);
+    plusMinus(arr);
 
-  return 0;
-
+    return 0;
 }
 
-vector<string> split_string(string input_string) {
-  string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-      return x == y and x == ' ';
+string ltrim(const string &str) {
+    string s(str);
 
-      });
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
 
-  input_string.erase(new_end, input_string.end());
-
-  while (input_string[input_string.length() - 1] == ' ') {
-    input_string.pop_back();
-
-  }
-
-  vector<string> splits;
-  char delimiter = ' ';
-
-  size_t i = 0;
-  size_t pos = input_string.find(delimiter);
-
-  while (pos != string::npos) {
-    splits.push_back(input_string.substr(i, pos - i));
-
-    i = pos + 1;
-    pos = input_string.find(delimiter, i);
-
-  }
-
-  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
-
-  return splits;
-
+    return s;
 }
 
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
