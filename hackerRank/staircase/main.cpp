76be9e36cb01e8e3867267e12cdd44582cf10256
jguzman4 <jeffg@liatrio.com>
2022-05-10 17:23:45 -0700

    staircase

--- hackerRank/staircase/main.cpp
+++ hackerRank/staircase/main.cpp
@@ -2,9 +2,17 @@
 
 using namespace std;
 
-// Complete the staircase function below.
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
 void staircase(int n) {
-  
+ 
   for (int i = 0; i < n; i++){
     for (int j = 0; j < n; j++){
       if (j < n-i-1)
@@ -14,16 +22,38 @@ void staircase(int n) {
     }
     cout << endl;
   }
-
 }
 
 int main()
 {
-  int n;
-  cin >> n;
-  cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    string n_temp;
+    getline(cin, n_temp);
 
-  staircase(n);
+    int n = stoi(ltrim(rtrim(n_temp)));
 
-  return 0;
+    staircase(n);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
 }
