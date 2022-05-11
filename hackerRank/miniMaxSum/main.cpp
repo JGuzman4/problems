c95e966e8b53db50d26831efb2bd94258a3a768c
jguzman4 <jeffg@liatrio.com>
2022-05-10 17:29:47 -0700

    miniMaxSum

--- hackerRank/miniMaxSum/main.cpp
+++ hackerRank/miniMaxSum/main.cpp
@@ -2,9 +2,16 @@
 
 using namespace std;
 
-vector<string> split_string(string);
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
 
-// Complete the miniMaxSum function below.
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
 void miniMaxSum(vector<int> arr) {
 
   long double min = std::numeric_limits<double>::max();
@@ -28,54 +35,64 @@ void miniMaxSum(vector<int> arr) {
 
   cout.precision(12);
   cout << min << " " << max << endl;
-
 }
 
 int main()
 {
-  string arr_temp_temp;
-  getline(cin, arr_temp_temp);
 
-  vector<string> arr_temp = split_string(arr_temp_temp);
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
 
-  vector<int> arr(5);
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
 
-  for (int i = 0; i < 5; i++) {
-    int arr_item = stoi(arr_temp[i]);
+    vector<int> arr(5);
 
-    arr[i] = arr_item;
-  }
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
 
-  miniMaxSum(arr);
+        arr[i] = arr_item;
+    }
 
-  return 0;
+    miniMaxSum(arr);
+
+    return 0;
 }
 
-vector<string> split_string(string input_string) {
-  string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
-      return x == y and x == ' ';
-      });
+string ltrim(const string &str) {
+    string s(str);
 
-  input_string.erase(new_end, input_string.end());
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
 
-  while (input_string[input_string.length() - 1] == ' ') {
-    input_string.pop_back();
-  }
+    return s;
+}
 
-  vector<string> splits;
-  char delimiter = ' ';
+string rtrim(const string &str) {
+    string s(str);
 
-  size_t i = 0;
-  size_t pos = input_string.find(delimiter);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
 
-  while (pos != string::npos) {
-    splits.push_back(input_string.substr(i, pos - i));
+    return s;
+}
 
-    i = pos + 1;
-    pos = input_string.find(delimiter, i);
-  }
+vector<string> split(const string &str) {
+    vector<string> tokens;
 
-  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+    string::size_type start = 0;
+    string::size_type end = 0;
 
-  return splits;
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
 }
