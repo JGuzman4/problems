db36ef738fe919da08f215e835d708e188581b99
jguzman4 <jeffg@liatrio.com>
2022-05-30 15:02:30 -0700

    invertBinaryTree

---
+++ leetCode/invertBinaryTree/main.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+
+        if (root == NULL) {
+            return NULL;
+        }
+
+        TreeNode* right = invertTree(root->right);
+        TreeNode* left = invertTree(root->left);
+
+        root->right = left;
+        root->left = right;
+
+        return root;
+    }
+};
+
+int main() {
+    string nums_str;
+    getline(cin, nums_str);
+
+    vector<int> nums;
+
+    stringstream ss(nums_str);
+    char delim = ' ';
+    string s;
+
+    while (getline(ss, s, delim)) {
+        nums.push_back(stoi(s));
+    }
+
+    Solution sol;
+
+    vector<int> sums = sol.invertTree(root);
+
+    for (int i = 0; i < sums.size(); i++) {
+        cout << sums[i];
+        if (i != sums.size() - 1) {
+            cout << " ";
+        }
+    }
+    cout << "\n";
+    return 0;
+}
