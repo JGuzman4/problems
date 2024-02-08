acf5f22d852ec84c706152c37386a5248a96a2bb
jguzman4 <jguz54057@gmail.com>
2024-02-07 21:50:59 -0800

    working bot saves princess

--- hackerRank/artificial-intelligence/bot-saves-princess/main.go
+++ hackerRank/artificial-intelligence/bot-saves-princess/main.go
@@ -8,26 +8,6 @@ import (
 	"strings"
 )
 
-func getMarioLocation() {
-}
-
-func readMap(n int) [][]rune {
-	grid := make([][]rune, n)
-	for i := 0; i < n; i++ {
-		grid[i] = make([]rune, n)
-	}
-	reader := bufio.NewReader(os.Stdin)
-	for i := 0; i < n; i++ {
-		line, _ := reader.ReadString('\n')
-		for j, char := range line {
-			if char != '\n' {
-				grid[i][j] = char
-			}
-		}
-	}
-	return grid
-}
-
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	nString, err := reader.ReadString('\n')
@@ -41,8 +21,38 @@ func main() {
 		fmt.Println("could not convert string to int", err)
 		return
 	}
-	grid := readMap(n)
-	for _, row := range grid {
-		fmt.Println(string(row))
+	var marioLoc [2]int
+	var peachLoc [2]int
+	for i := 0; i < n; i++ {
+		line, _ := reader.ReadString('\n')
+		for j, char := range line {
+			if char == 'm' {
+				marioLoc[0] = i
+				marioLoc[1] = j
+			}
+			if char == 'p' {
+				peachLoc[0] = i
+				peachLoc[1] = j
+			}
+		}
+	}
+
+	fmt.Println("mario location: ", marioLoc[0], ", ", marioLoc[1])
+	fmt.Println("peach location: ", peachLoc[0], ", ", peachLoc[1])
+	for marioLoc[0] > peachLoc[0] {
+		fmt.Println("UP")
+		marioLoc[0] = marioLoc[0] - 1
+	}
+	for marioLoc[0] < peachLoc[0] {
+		fmt.Println("DOWN")
+		marioLoc[0] = marioLoc[0] + 1
+	}
+	for marioLoc[1] > peachLoc[1] {
+		fmt.Println("LEFT")
+		marioLoc[1] = marioLoc[1] - 1
+	}
+	for marioLoc[1] < peachLoc[1] {
+		fmt.Println("RIGHT")
+		marioLoc[1] = marioLoc[1] + 1
 	}
 }
